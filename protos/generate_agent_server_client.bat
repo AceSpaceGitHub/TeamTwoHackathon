bb2e4dd56334d538d94e8e9f7ac697f279794deb
gabby thomas <gabrient@gmail.com>
2021-10-21 13:33:35 -0700

    nvm. getting frontend to talk to grpc service is opening rabbit holes.

--- protos/generate_agent_server_client.bat
+++
@@ -1,3 +0,0 @@
-python -m grpc_tools.protoc -I./ --python_out=../Backend/agent-server --grpc_python_out=../Backend/agent-server agent.proto
-
-rem TODO generate JS client from same proto file into frontend directory
