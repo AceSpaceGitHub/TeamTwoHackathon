392cb04d07bd87b1075827c8a4b31b346c865cb7
TheLuckyMojo <ethanmorales0730@gmail.com>
2021-10-20 22:49:11 -0700

    Initial UI mock up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,88 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom';
+import { Button, FormControl, InputLabel, MenuItem, Select, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField } from '@mui/material';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class NameForm extends React.Component<any, any> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      targets: 8,
+      attackers: 13
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event: any) {
+    this.setState({ value: event.target.value });
+  }
+
+  render() {
+    function createData(
+      name: string,
+      status: string,
+    ) {
+      return { name, status };
+    }
+
+    const rows = [
+      createData('Ship 1', 'None'),
+      createData('Ship 2', 'Disabled'),
+      createData('Ship 3', 'Disabled'),
+      createData('Ship 4', 'Destroyed'),
+      createData('Ship 5', 'Destroyed'),
+    ];
+    return (
+      <div>
+        <form>
+          <TextField id="outlined-basic" label="Number of targets:" variant="outlined" margin="normal" value={this.state.targets} onChange={this.handleChange} />
+          <br />
+          <TextField id="outlined-basic" label="Number of attackers:" variant="outlined" margin="normal" value={this.state.attackers} onChange={this.handleChange} />
+          <br />
+          <Button variant="contained" onClick={() => { alert(this.state.targets + ' targets and ' + this.state.attackers + ' attackers') }}>Sumulate</Button>
+        </form>
+
+        <TableContainer>
+          <Table aria-label="a dense table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Target</TableCell>
+                <TableCell>Status</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow
+                  key={row.name}
+                >
+                  <TableCell>{row.name}</TableCell>
+                  <TableCell>
+                    <FormControl fullWidth>
+                      <InputLabel id="demo-simple-select-label">Status</InputLabel>
+                      <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                      >
+                        <MenuItem>None</MenuItem>
+                        <MenuItem>Disabled</MenuItem>
+                        <MenuItem>Destroyed</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    );
+  }
 }
 
-export default App;
+ReactDOM.render(
+  <NameForm />,
+  document.getElementById('root')
+);
+
+export default NameForm;
