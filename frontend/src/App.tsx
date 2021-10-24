308d5521d1df396136b16b65dcedd71a3a9887d2
TheLuckyMojo <ethanmorales0730@gmail.com>
2021-10-24 00:50:28 -0700

    Percentage to protect table

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,62 @@
-import React from 'react';
-import './App.css';
-import ReactDOM from 'react-dom';
-import { Button, FormControl, InputLabel, MenuItem, Paper, Select, Table, TableBody, TableCell, TableContainer, TableRow, TextField } from '@mui/material';
+import React from "react";
+import "./App.css";
+import ReactDOM from "react-dom";
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Paper,
+  Select,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  TextField,
+} from "@mui/material";
 
 class NameForm extends React.Component<any, any> {
   constructor(props: any) {
     super(props);
     this.state = {
-      missiles: ''
+      missiles: "",
     };
 
-    this.handleChange = this.handleChange.bind(this);
+    this.handleTextChange = this.handleTextChange.bind(this);
   }
 
-  handleChange(event: any) {
+  handleTextChange(event: any) {
     this.setState({ missiles: event.target.value });
   }
 
+  // handleSelectChange(event: any) {
+  //   this.setState((prevState: { shipDamage: any }) => ({
+  //     shipDamage: [...prevState.shipDamage, event.target.value],
+  //   }));
+  // }
+
   render() {
-    //Plug this missilesNum array into this line `{ships.map((row) => (` to dynamically update table rows
-    const missilesNum: string[] = [];
-    for (let k = 0; k < Number(this.state.missiles); k++) {
-      var displayVal = k + 1
-      missilesNum[k] = 'Ship ' + displayVal
+    function createData(name: string, damage: string) {
+      return { name, damage };
     }
 
-    const ships = ['Ship 1', 'Ship 2', 'Ship 3', 'Ship 4', 'Ship 5', 'Ship 6']
+    const ships = [
+      createData("Ship 1", ""),
+      createData("Ship 2", ""),
+      createData("Ship 3", ""),
+      createData("Ship 4", ""),
+      createData("Ship 5", ""),
+    ];
+
+    //Plug this missilesNum array into
+    //line `{ships.map((row) => (` to dynamically update table rows
+    const missilesNum: string[] = [];
+    for (let k = 0; k < Number(this.state.missiles); k++) {
+      var displayVal = k + 1;
+      missilesNum[k] = "Ship " + displayVal;
+    }
+
     return (
       <div>
         <TextField
@@ -36,7 +67,7 @@ class NameForm extends React.Component<any, any> {
           type="number"
           value={this.state.missiles}
           InputLabelProps={{ shrink: true }}
-          onChange={this.handleChange}
+          onChange={this.handleTextChange}
         />
 
         <TableContainer component={Paper}>
@@ -44,17 +75,21 @@ class NameForm extends React.Component<any, any> {
             <TableBody>
               {ships.map((row) => (
                 <TableRow
-                  key={row}
-                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  key={row.name}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
                 >
                   <TableCell component="th" scope="row">
-                    {row}
+                    {row.name}
                   </TableCell>
                   <TableCell>
                     <FormControl fullWidth>
-                      <InputLabel id="demo-simple-select-label">Status</InputLabel>
+                      <InputLabel id="demo-simple-select-label">
+                        Status
+                      </InputLabel>
                       <Select
+                        value={row.damage}
                         labelId="demo-simple-select-label"
+                        // onChange={this.handleSelectChange}
                         id="demo-simple-select"
                       >
                         <MenuItem>No Damage</MenuItem>
@@ -69,15 +104,52 @@ class NameForm extends React.Component<any, any> {
           </Table>
         </TableContainer>
         <br />
-        <Button variant="contained" onClick={() => { alert(this.state.missiles + ' missiles') }}>Simulate</Button>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableBody>
+              {ships.map((row) => (
+                <TableRow
+                  key={row.name}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  {ships.forEach((element) => {
+                    if (element.name !== row.name) {
+                      var labelString =
+                        "Percent to protect " + element.name + ":";
+                      {
+                        <TableCell>
+                          <TextField
+                            label="percentage to protect"
+                            variant="outlined"
+                            type="number"
+                            InputLabelProps={{ shrink: true }}
+                          />
+                        </TableCell>;
+                      }
+                    }
+                  })}
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <br />
+        <Button
+          variant="contained"
+          onClick={() => {
+            alert(this.state.missiles + " missiles");
+          }}
+        >
+          Simulate
+        </Button>
       </div>
     );
   }
 }
 
-ReactDOM.render(
-  <NameForm />,
-  document.getElementById('root')
-);
+ReactDOM.render(<NameForm />, document.getElementById("root"));
 
 export default NameForm;
